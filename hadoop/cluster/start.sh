26db6ebb0e314e12b71237ddf20e4e46880d6d96
Wei Lin <Wei1234c@gmail.com>
2015-09-08 17:08:35 +0800

    modify start.sh and dockerfile for hadoop cluster

--- hadoop/cluster/start.sh
+++ hadoop/cluster/start.sh
@@ -1,32 +1,28 @@
 # Start containers
-docker run -dit -P --name=master hadoop_distributed
-docker run -dit -P --name=slave1 --net=container:master hadoop_distributed
-docker run -dit -P --name=slave2 --net=container:master hadoop_distributed
+docker run -dit -P --name=master -v /data:/data hadoop_distributed bash
+docker run -dit -P --name=slave1 -v /data:/data hadoop_distributed bash
+docker run -dit -P --name=slave2 -v /data:/data hadoop_distributed bash
 
-# Get IP addresses
-export master_ip="$(docker inspect master | grep IPAddress | sed 's/["IPAddress",\:,\s,\",\,]//g')"
-export slave1_ip="$(docker inspect slave1 | grep IPAddress | sed 's/["IPAddress",\:,\s,\",\,]//g')"
-export slave2_ip="$(docker inspect slave2 | grep IPAddress | sed 's/["IPAddress",\:,\s,\",\,]//g')"
+# Get containers' IP addresses
+master_ip="$(docker inspect master | grep IPAddress | sed 's/["IPAddress",\:,\s,\",\,]//g')"
+slave1_ip="$(docker inspect slave1 | grep IPAddress | sed 's/["IPAddress",\:,\s,\",\,]//g')"
+slave2_ip="$(docker inspect slave2 | grep IPAddress | sed 's/["IPAddress",\:,\s,\",\,]//g')"
 
-echo "${master_ip} master"
-echo "${slave1_ip} slave1"
-echo "${slave2_ip} slave2"
+# Add hosts
+echo "${master_ip} master" >  /data/etc/hosts
+echo "${slave1_ip} slave1" >> /data/etc/hosts
+echo "${slave2_ip} slave2" >> /data/etc/hosts
 
-# Set Hosts
-docker exec master echo "${master_ip} master" >> /etc/hosts
-docker exec master echo "${slave1_ip} slave1" >> /etc/hosts
-docker exec master echo "${slave2_ip} slave2" >> /etc/hosts
-
-docker exec slave1 echo "${master_ip} master" >> /etc/hosts
-docker exec slave1 echo "${slave1_ip} slave1" >> /etc/hosts
-docker exec slave1 echo "${slave2_ip} slave2" >> /etc/hosts
-
-docker exec slave2 echo "${master_ip} master" >> /etc/hosts
-docker exec slave2 echo "${slave1_ip} slave1" >> /etc/hosts
-docker exec slave2 echo "${slave2_ip} slave2" >> /etc/hosts
+# Test networking
+docker exec master ping -c 1 slave1
+docker exec master ping -c 1 slave2
+docker exec slave1 ping -c 1 master
+docker exec slave2 ping -c 1 master
 
 # Startup cluster
-# HADOOP_HOME=/usr/local/hadoop
-# docker exec master ${HADOOP_HOME}/bin/hdfs namenode -format
-# docker exec master /etc/bootstrap.sh -d
+HADOOP_HOME=/usr/local/hadoop
+docker exec master ${HADOOP_HOME}/bin/hdfs namenode -format
+docker exec slave1 ${HADOOP_HOME}/bin/hdfs datanode -format
+docker exec slave2 ${HADOOP_HOME}/bin/hdfs datanode -format
+docker exec master ${HADOOP_HOME}/sbin/start-all.sh
 
