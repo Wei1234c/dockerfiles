6b6e79989d50f8536ab007f92f2aae78dab92189
Wei Lin <Wei1234c@gmail.com>
2015-09-08 17:48:25 +0800

    add remove_images.sh remove_containers.sh

---
+++ build_images.sh
@@ -0,0 +1,11 @@
+docker build -t oracle-java8 java/oracle-java8
+docker build -t sshd sshd
+docker build -t oracle-java8_sshd java_sshd
+docker build -t python python
+docker build -t nodejs nodejs
+docker build -t mysql mysql
+docker build -t mongodb mongodb
+docker build -t apache apache
+docker build -t tomcat tomcat
+docker build -t hadoop_pseudo-distributed hadoop/pseudo-distributed
+docker build -t hadoop_distributed hadoop/cluster/node
