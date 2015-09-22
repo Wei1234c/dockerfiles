02202ff229062938aae68fdb9647b00d7215a20e
root <root@black-pearl.(none)>
2015-09-22 12:07:36 +0800

    update hadoop pseudo mode dockerfile and test.sh

--- ARMv7/hadoop/pseudo-distributed/test.sh
+++ ARMv7/hadoop/pseudo-distributed/test.sh
@@ -3,15 +3,19 @@ docker attach hadoop
 /etc/bootstrap.sh
 
 cd ${HADOOP_HOME}/bin
+
+./hadoop fs -rm -r -f input/test
 ./hadoop fs -mkdir input/test
 ./hadoop fs -ls input/test
+
+./hadoop fs -rm -r -f output
 ./hadoop fs -ls output
+
 ./hadoop fs -copyFromLocal /data/hadoop_input/* input/test/
 ./hadoop fs -ls input/test
 
-./hadoop jar /data/hadoop_input/WordCount.jar test.WordCount  input/test  output
+./hadoop jar /data/WordCount.jar WordCount  input/test  output
 
 ./hadoop fs -ls output
-./hadoop fs -cat output/_SUCCESS
-./hadoop fs -cat output/part-00000
+./hadoop fs -cat output/part*
 
