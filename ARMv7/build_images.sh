a162b4160c51b51ec581983bb9ad50f45e657a82
wei1234c <wei1234c@gmail.com>
2015-10-01 11:50:49 +0800

    remove redundent statements in dockerfiles

--- ARMv7/build_images.sh
+++ ARMv7/build_images.sh
@@ -2,16 +2,17 @@
 
 docker pull armv7/armhf-ubuntu:14.04
 docker tag armv7/armhf-ubuntu:14.04 ubuntu:14.04
-docker tag ubuntu:14.04 ubuntu:latest
+docker tag armv7/armhf-ubuntu:14.04 ubuntu:latest
 
-docker build -t wei1234c/oracle-java8_armv7 /dockerfiles/ARMv7/java/oracle-java8
-docker build -t wei1234c/oracle-java8_sshd_armv7 /dockerfiles/ARMv7/java_sshd
+docker build -t wei1234c/ubuntu_armv7 /dockerfiles/ARMv7/ubuntu
+docker build -t wei1234c/sshd_armv7 /dockerfiles/ARMv7/sshd
+docker build -t wei1234c/java_armv7 /dockerfiles/ARMv7/java/oracle-java8
+docker build -t wei1234c/java_sshd_armv7 /dockerfiles/ARMv7/java_sshd
 docker build -t wei1234c/php_armv7:5 /dockerfiles/ARMv7/php/5
 docker build -t wei1234c/php_armv7:cli /dockerfiles/ARMv7/php/5.6
 docker build -t wei1234c/python_armv7 /dockerfiles/ARMv7/python
 docker build -t wei1234c/nodejs_armv7 /dockerfiles/ARMv7/nodejs
 docker build -t wei1234c/jupyter-notebook_armv7 /dockerfiles/ARMv7/notebook
-docker build -t wei1234c/sshd_armv7 /dockerfiles/ARMv7/sshd
 docker build -t wei1234c/haproxy_armv7 /dockerfiles/ARMv7/haproxy/1.5
 docker build -t wei1234c/apache_armv7 /dockerfiles/ARMv7/apache
 docker build -t wei1234c/tomcat_armv7 /dockerfiles/ARMv7/tomcat
@@ -23,25 +24,5 @@ docker build -t wei1234c/hadoop_pseudo-distributed_armv7 /dockerfiles/ARMv7/hado
 docker build -t wei1234c/hbase_armv7 /dockerfiles/ARMv7/hbase
 docker build -t wei1234c/hive_armv7 /dockerfiles/ARMv7/hive
 # docker build -t wei1234c/hadoop_full-distributed_armv7 /dockerfiles/ARMv7/hadoop/cluster/node
-
-docker push wei1234c/oracle-java8_armv7
-docker push wei1234c/oracle-java8_sshd_armv7
-docker push wei1234c/php_armv7:5
-docker push wei1234c/php_armv7:cli
-docker push wei1234c/python_armv7
-docker push wei1234c/nodejs_armv7
-docker push wei1234c/jupyter-notebook_armv7
-docker push wei1234c/sshd_armv7
-docker push wei1234c/haproxy_armv7
-docker push wei1234c/apache_armv7
-docker push wei1234c/tomcat_armv7
-docker push wei1234c/lamp_armv7
-docker push wei1234c/mongodb_armv7
-docker push wei1234c/mysql_armv7
-docker push wei1234c/lubuntu-desktop_armv7
-docker push wei1234c/hadoop_pseudo-distributed_armv7
-docker push wei1234c/hbase_armv7
-docker push wei1234c/hive_armv7
-# docker push wei1234c/hadoop_full-distributed_armv7
 
 dcleanup
--- ARMv7/hadoop/build_hadoop_images.sh
+++
@@ -1,5 +0,0 @@
-rm ~/nohup.out
-
-cd /dockerfiles/ARMv7
-nohup docker build -t hadoop_pseudo-distributed hadoop/pseudo-distributed & 
-nohup docker build -t hadoop_full-distributed hadoop/cluster/node & 
