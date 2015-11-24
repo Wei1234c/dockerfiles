a634200db70d0a6423c0c5db8386a7e8125b9003
Wei Lin <wei1234c@gmail.com>
2015-11-25 01:27:53 +0800

    20151125

--- ARMv7/cgminer/start.sh
+++ ARMv7/cgminer/start.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
 CGMiner_HOME=/usr/local/cgminer
-
-
-INDEX=1
-
-# Start containers
-docker run -dit -P --name=miner_${INDEX} -v /data:/data wei1234c/cgminer_armv7 
-
-# Get containers' IP addresses
-miner_${INDEX}_ip="$(docker inspect -f {{.NetworkSettings.IPAddress}} miner_${INDEX})" 
-
-# start sshd
-docker exec miner_${INDEX} service ssh start
-
-# start mining
 POOL=stratum+tcp://stratum.bitcoin.cz:3333
-USERNAME=WeiLin.worker1 
-PASSWORD=1
 
-docker exec miner_${INDEX} /bin/sh -c "/usr/local/cgminer/cgminer -o ${POOL} -u ${USERNAME} -p ${PASSWORD}"
+for ((INDEX=1; INDEX <= 3; INDEX++))
+do
+
+	# INDEX=1
+
+	# Start containers
+	docker run -di -P --name=miner_${INDEX} -v /data:/data wei1234c/cgminer_armv7 
+
+	# start sshd
+	docker exec miner_${INDEX} service ssh start
+
+	# start mining
+	
+	USERNAME=WeiLin.worker${INDEX}  
+	PASSWORD=${INDEX} 
+
+	docker exec miner_${INDEX} /bin/sh -c "/usr/local/cgminer/cgminer -o ${POOL} -u ${USERNAME} -p ${PASSWORD}"
+
+done
+
+
 # docker exec miner_${INDEX} /bin/sh -c "/usr/local/cgminer/cgminer --bmsc-options 115200:0.57 -o ${POOL} -u ${USERNAME} -p ${PASSWORD} --bmsc-voltage 0800 --bmsc-freq 1286"
-# ./cgminer --userpass userID.workerID:any-password --url stratum+tcp://stratum.bitcoin.cz:3333
-
-# Clean up
-# docker stop $(docker ps -q)
-# docker rm $(docker ps -aq)
-
-
 
