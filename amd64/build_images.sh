5f597065fe674f9120c4bda7bced7f2bc643dac3
Wei Lin <wei1234c@gmail.com>
2017-03-04 12:15:47 +0800

    add micropython

--- amd64/build_images.sh
+++ amd64/build_images.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
- 
-docker build -t wei1234c/esp8266 /dockerfiles/amd64/esp8266
-docker build -t wei1234c/micropython /dockerfiles/amd64/micropython
+docker build -t wei1234c/esp8266 /home/wei/Dropbox/Coding/Git/dockerfiles/amd64/esp8266
+docker build -t wei1234c/micropython /home/wei/Dropbox/Coding/Git/dockerfiles/amd64/micropython
 
  
 docker push wei1234c/esp8266
