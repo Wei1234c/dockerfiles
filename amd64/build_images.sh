f27aa2596141d8447144cd991e76381de9514710
Wei Lin <wei1234c@gmail.com>
2017-11-05 01:15:37 +0800

    add Dockerfile for Keran on amd64

--- amd64/build_images.sh
+++ amd64/build_images.sh
@@ -2,10 +2,12 @@
 
 docker build -t wei1234c/esp8266 /home/wei/Dropbox/Coding/Git/dockerfiles/amd64/esp8266
 docker build -t wei1234c/micropython /home/wei/Dropbox/Coding/Git/dockerfiles/amd64/micropython
+docker build -t wei1234c/keras /home/wei/Dropbox/Coding/Git/dockerfiles/amd64/keras
 
  
 docker push wei1234c/esp8266
 docker push wei1234c/micropython
+docker push wei1234c/keras
 
 call drmc
 call dcleanup
