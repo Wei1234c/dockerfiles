bd0dc2ad43f7334a11d5794055eba0e7409175f3
Wei Lin <Wei1234c@gmail.com>
2018-03-13 17:19:15 +0800

    add dockerfile for ROS.

---
+++ ARMv7/ros/ROS/run_ros.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+docker run -it --rm \
+	--net=host \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-e DISPLAY=unix$DISPLAY \
+	--cap-add=SYS_ADMIN \
+	--device /dev/snd \
+	--device /dev/dri \
+	-v /dev/shm:/dev/shm \
+	-p 11311:11311 \
+	wei1234c/ros_ubuntu_armv7
+
+
+#	--name=ROS \
+
+# docker run -it --rm \
+#        -e DISPLAY=$DISPLAY \
+#        -v /tmp/.X11-unix:/tmp/.X11-unix \
+#        wei1234c/ros_ubuntu_amd64
